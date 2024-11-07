de4b137e73db75e58374a2baceb424039547d34a
Danl245 <danya.volodchenko.22@icloud.com>
2024-11-07 22:42:34 +0300

    Update untitled5.py

--- untitled5.py
+++ untitled5.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1WqFYkh1asR5MZX3yKdo0iFVQGYHKbuuh
 """
 
-pip install streamlit
+
 
 import streamlit as st
 import pandas as pd
